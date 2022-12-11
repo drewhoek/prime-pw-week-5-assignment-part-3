eb1f09c0899d182189964afb8cf9bf31bbddeb42
Drew Hoek <drewh815@gmail.com>
2022-12-11 14:18:58 -0600

    Added 6 albums using addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,4 +13,10 @@ function addToCollection ( title, artist, yearPuplished ) {
 }
 
 console.log( addToCollection( 'To Pimp a Butterfly', 'Kendrick Lamar', '2015' ) );
-console.log( collection );
+console.log( collection ); // To check if first album was added.
+console.log( addToCollection( 'Circles', 'Mac Miller', '2020' ) );
+console.log( addToCollection( 'Swimming', 'Mac Miller', '2018' ) );
+console.log( addToCollection( 'The Very Best of the Eagles', 'Eagles', '2003' ) );
+console.log( addToCollection( 'Trauma', 'I Prevail', '2019' ) );
+console.log( addToCollection( 'Mr. Morale and the Big Steppers', 'Kendrick Lamar', '2022' ) );
+console.log( 'My album collection is:', collection );
