cbf03f0868a29d390d84bd12c550df74d25df5d5
Drew Hoek <drewh815@gmail.com>
2022-12-11 14:01:18 -0600

    Completed addToCollection Function and tested

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,16 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection ( title, artist, yearPuplished ) {
+    let record = {
+        title: title,
+        artist: artist,
+        yearPuplished: yearPuplished,
+    }
+    collection.push(record);
+    return record;
+}
+
+console.log( addToCollection( 'To Pimp a Butterfly', 'Kendrick Lamar', '2015' ) );
+console.log( collection );
